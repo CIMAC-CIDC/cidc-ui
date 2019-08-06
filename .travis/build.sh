532db6121b8cc5ef7bc45adebbaad716c23aa16a
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-08-06 09:31:43 -0400

    Update .travis.yml to deploy built site to GCS

---
+++ .travis/build.sh
@@ -0,0 +1,8 @@
+# !/usr/bin/bash
+#
+# Create a production build of the CIDC Portal UI.
+
+# TODO: generate up-to-date shipping manifest templates 
+# and move these into public/static before running build
+
+npm build
