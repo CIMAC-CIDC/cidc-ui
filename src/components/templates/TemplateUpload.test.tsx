e86ce34fc0876e4ee315b158b747f2ac97fefa4a
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-09-10 12:10:12 -0400

    Stub out submission smoketest

--- src/components/templates/TemplateUpload.test.tsx
+++ src/components/templates/TemplateUpload.test.tsx
@@ -8,7 +8,7 @@ import {
 import TemplateUpload from "./TemplateUpload";
 import { XLSX_MIMETYPE } from "../../util/constants";
 import Auth, { AuthContext } from "../../auth/Auth";
-import { getManifestValidationErrors } from "../../api/api";
+import { getManifestValidationErrors, uploadManifest } from "../../api/api";
 jest.mock("../../api/api");
 
 const TOKEN = "BLAH";
@@ -62,7 +62,7 @@ test("manifest validation", async () => {
     // Select a manifest type. Material UI components make this
     // difficult: we need to first click on the select component,
     // then click on the dropdown option that subsequently appears.
-    selectValueMUI(renderResult, manifestTypeSelect, "PBMC");
+    await selectValueMUI(renderResult, manifestTypeSelect, "PBMC");
     expectNoValidationsDisplayed();
     expect(submitButton).toHaveAttribute("disabled");
 
@@ -81,16 +81,47 @@ test("manifest validation", async () => {
         expect(submitButton).toHaveAttribute("disabled");
 
         await waitForElement(() => queryByTestId(element)!);
+        expect(queryByTestId("no-selection")).toBeNull();
     }
 
     // Check that validation errors show up and submit button is still disabled
     const errs = ["a", "b", "c"];
     await doValidationRequest(errs, "errors");
     errs.map(e => expect(getByText(e)).toBeInTheDocument());
+    expect(queryByTestId("valid-manifest")).toBeNull();
     expect(submitButton).toHaveAttribute("disabled");
 
     // Check that valid message shows up and submit button is enabled
     await doValidationRequest([], "valid-manifest");
     expect(queryByTestId("valid-manifest")).toBeInTheDocument();
+    expect(queryByTestId("errors")).toBeNull();
     expect(submitButton).not.toHaveAttribute("disabled");
+});
+
+test("manifest submission", async () => {
+    const renderResult = renderWithMockedAuthContext();
+    const { getByTestId, getByText } = renderResult;
+    const manifestTypeSelect = getByTestId("manifest-type-select");
+    const manifestFileInput = getByTestId("manifest-file-input");
+    const submitButton = getByTestId("submit-button");
+
+    // Select a manifest type
+    selectValueMUI(renderResult, manifestTypeSelect, "PBMC");
+
+    // Select a file to upload and wait for validations to complete
+    getManifestValidationErrors.mockResolvedValue([]);
+    fireEvent.click(manifestFileInput);
+    const fakePBMCFile = new File(["foo"], "pbmc.xlsx", {
+        type: XLSX_MIMETYPE
+    });
+    fireEvent.change(manifestFileInput, {
+        target: { files: [fakePBMCFile] }
+    });
+    await waitForElement(() => getByTestId("valid-manifest")!);
+
+    // Submit the manifest
+    uploadManifest.mockResolvedValue({
+        metadata_json_patch: { lead_organization_study_id: "CIMAC-12345" }
+    });
+    fireEvent.click(submitButton);
 });
