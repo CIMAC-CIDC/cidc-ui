02ae8a9cf40c9042833b04180989f180e31aa439
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-09-10 13:33:28 -0400

    Revert unnecessary bracket addition

--- src/components/templates/utils.ts
+++ src/components/templates/utils.ts
@@ -6,7 +6,6 @@ export const allNames = {
 };
 
 export function onValueChange(setState: (v: string | undefined) => void) {
-    return (e: React.ChangeEvent<HTMLSelectElement>) => {
+    return (e: React.ChangeEvent<HTMLSelectElement>) =>
         setState(e.target.value);
-    };
 }
