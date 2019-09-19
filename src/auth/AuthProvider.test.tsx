b4b83e73083afab85667767f06e49b40b03a995b
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-09-19 08:27:46 -0400

    Stub out tests

---
+++ src/auth/AuthProvider.test.tsx
@@ -0,0 +1,19 @@
+it("triggers login when the user is not logged in", () => {
+    expect(1).toBe(2);
+});
+
+it("doesn't trigger login on public endpoints", () => {
+    expect(1).toBe(2);
+});
+
+it("handles requests to '/callback' as expected", () => {
+    expect(1).toBe(2);
+});
+
+it("handles requests to '/logout' as expected", () => {
+    expect(1).toBe(2);
+});
+
+it("handles silent auth as expected", () => {
+    expect(1).toBe(2);
+});
