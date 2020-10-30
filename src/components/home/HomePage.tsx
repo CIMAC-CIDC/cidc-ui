3b770d479a6c3ed2b1f6989957715a9a01e55c7c
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-10-30 09:20:31 -0400

    Add data portal stats to HomePage

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,83 +1,175 @@
 import * as React from "react";
 import {
     Typography,
-    Link,
-    List,
-    ListItem,
-    ListItemText,
-    ListItemAvatar,
-    Divider
+    Grid,
+    Button,
+    Box,
+    Card,
+    CardContent
 } from "@material-ui/core";
-import {
-    Search,
-    AccountCircle,
-    Security,
-    TableChart
-} from "@material-ui/icons";
-import history from "../identity/History";
-import { UserContext } from "../identity/UserProvider";
 import { useRootStyles } from "../../rootStyles";
+import { getDataOverview, IDataOverview } from "../../api/api";
+import filesize from "filesize";
+import {
+    Assignment,
+    AssignmentOutlined,
+    FileCopyOutlined,
+    LocationOnOutlined,
+    OpacityOutlined,
+    OpenInNewOutlined,
+    PersonOutlined,
+    StorageOutlined
+} from "@material-ui/icons";
+import { RouteComponentProps } from "react-router-dom";
+import { colors } from "../../rootStyles";
 
-const ListLink: React.FunctionComponent<{
-    icon: React.ReactElement;
-    href: string;
-}> = props => (
-    <ListItem button onClick={() => history.push(props.href)}>
-        <ListItemAvatar>{props.icon}</ListItemAvatar>
-        <ListItemText>{props.children}</ListItemText>
-    </ListItem>
-);
-
-const HomePage: React.FunctionComponent = () => {
+const HomePage: React.FunctionComponent<RouteComponentProps> = ({
+    history
+}) => {
     const classes = useRootStyles();
-    const user = React.useContext(UserContext);
+
+    const [dataOverview, setDataOverview] = React.useState<
+        IDataOverview | undefined
+    >(undefined);
+    React.useEffect(() => {
+        getDataOverview().then(d => setDataOverview(d));
+    }, []);
 
     return (
-        <div className={classes.centeredPage} style={{ maxWidth: 700 }}>
-            <Typography
-                variant="h4"
-                style={{ textAlign: "center" }}
-                gutterBottom
-            >
-                Welcome to the CIMAC-CIDC Data Portal
-            </Typography>
-            <Typography variant="subtitle1" gutterBottom>
-                This site serves as the central data coordination hub for the{" "}
-                <Link href="https://cimac-network.org">CIMAC-CIDC Network</Link>
-                , an initiative of the NCI Cancer Moonshot that provides
-                cutting-edge technology and expertise in genomic, proteomic, and
-                functional molecular analysis to enhance clinical trials in
-                cancer immune therapies.
-            </Typography>
-            <Divider style={{ margin: "2em 0" }} />
-            <Typography gutterBottom>On this site, you can</Typography>
-            <List>
-                <ListLink icon={<Search />} href="/browse-data">
-                    Browse ingested data
-                </ListLink>
-                {user && user.showAssays && (
-                    <ListLink icon={<TableChart />} href="/assays">
-                        Learn how to upload new assay data
-                    </ListLink>
-                )}
-                {user && user.showAnalyses && (
-                    <ListLink icon={<TableChart />} href="/analyses">
-                        Learn how to upload new analysis data
-                    </ListLink>
-                )}
-                {user && user.showManifests && (
-                    <ListLink icon={<TableChart />} href="/manifests">
-                        Upload a shipping/receiving manifest
-                    </ListLink>
-                )}
-                <ListLink icon={<AccountCircle />} href="/profile">
-                    View your account info and data access permissions
-                </ListLink>
-                <ListLink icon={<Security />} href="/privacy-security">
-                    Read our Privacy and Security notice
-                </ListLink>
-            </List>
-        </div>
+        <Grid
+            className={classes.centeredPage}
+            container
+            direction="column"
+            alignItems="stretch"
+        >
+            <Grid item>
+                <Box paddingTop={8} paddingBottom={12}>
+                    <Typography
+                        align="center"
+                        variant="h3"
+                        style={{
+                            fontWeight: "bold"
+                        }}
+                        gutterBottom
+                    >
+                        Cancer Immunologic Data Commons
+                    </Typography>
+                    <Typography
+                        align="center"
+                        variant="h4"
+                        color="textSecondary"
+                    >
+                        A hub for cutting-edge cancer immunotherapy data
+                    </Typography>
+                </Box>
+            </Grid>
+            <Grid item>
+                <Card>
+                    <CardContent>
+                        <Grid
+                            container
+                            justify="space-between"
+                            alignItems="baseline"
+                            wrap="nowrap"
+                        >
+                            {([
+                                [
+                                    "trials",
+                                    dataOverview?.num_trials,
+                                    AssignmentOutlined
+                                ],
+                                [
+                                    "participants",
+                                    dataOverview?.num_participants,
+                                    PersonOutlined
+                                ],
+                                [
+                                    "samples",
+                                    dataOverview?.num_samples,
+                                    LocationOnOutlined
+                                ],
+                                [
+                                    "assays",
+                                    dataOverview?.num_assays,
+                                    OpacityOutlined
+                                ],
+                                [
+                                    "files",
+                                    dataOverview?.num_files,
+                                    FileCopyOutlined
+                                ],
+                                [
+                                    "data",
+                                    dataOverview
+                                        ? filesize(dataOverview.num_bytes)
+                                        : undefined,
+                                    StorageOutlined
+                                ]
+                            ] as Array<
+                                [
+                                    string,
+                                    string | undefined,
+                                    typeof Assignment | undefined
+                                ]
+                            >).map(([label, value, Icon]) => {
+                                return (
+                                    <Grid key={label} item>
+                                        <Grid
+                                            container
+                                            alignItems="center"
+                                            spacing={1}
+                                        >
+                                            <Grid item>
+                                                {Icon && (
+                                                    <Icon
+                                                        style={{
+                                                            fontSize: "4.5rem",
+                                                            color:
+                                                                colors.logoDarkBlue
+                                                        }}
+                                                    />
+                                                )}
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography
+                                                    variant="h4"
+                                                    style={{
+                                                        fontSize: "1.6rem",
+                                                        fontWeight: "bold"
+                                                    }}
+                                                >
+                                                    {value || "-"}
+                                                </Typography>
+                                                <Typography
+                                                    variant="overline"
+                                                    style={{
+                                                        fontSize: "1rem"
+                                                    }}
+                                                >
+                                                    {label}
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                );
+                            })}
+                        </Grid>
+                        <Button
+                            fullWidth
+                            disableElevation
+                            size="medium"
+                            color="primary"
+                            variant="contained"
+                            onClick={() => history.push("/browse-data")}
+                            endIcon={<OpenInNewOutlined />}
+                            style={{ marginTop: "2rem" }}
+                        >
+                            explore the data
+                        </Button>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     );
 };
 
