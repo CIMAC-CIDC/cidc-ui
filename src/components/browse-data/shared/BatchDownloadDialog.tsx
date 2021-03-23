5fc9cafa7e29a00cdd50cec82eafb21a46852c9c
Jacob Lurye <jlurye@jimmy.harvard.edu>
2021-03-23 14:58:25 -0400

    Add button for downloading user data access reports to the UI

--- src/components/browse-data/shared/BatchDownloadDialog.tsx
+++ src/components/browse-data/shared/BatchDownloadDialog.tsx
@@ -33,7 +33,6 @@ const BatchDownloadDialog: React.FC<IBatchDownloadDialogProps> = ({
             const url = URL.createObjectURL(blob);
             const a = document.createElement("a");
             a.href = url;
-            a.download = "filelist.tsv";
             document.body.appendChild(a);
             a.click();
             URL.revokeObjectURL(url);
